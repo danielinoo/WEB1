d7e35262ded5555f1ceb064a520b9201169e948a
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-07-23 13:40:26 +0200

    server form mansendfile

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+//esempoi---------------------------------------------------
+
+
 //pagina di invio della form
 app.get('/formRegistrazione', (req, res) => {
 console.log("Mi hai chiesto la form di registrazione");
@@ -21,3 +25,37 @@ app.get('/gestiscidatiform', (req, res) => {
     console.log(req.query.fname);
     res.send("<html>  Buonaserata " + req.query.fname + " " + rep.query.fcognome + "</html" );
     });
+
+
+
+
+
+//esercizio1  --------------------------------------------
+
+app.get('/sendfile', (req, res) => {
+    console.log("Mi hai chiesto la form di registrazione");
+    res.sendFile("sendfile.html", { root: './htdoc' });
+    });
+
+
+
+// gestione urlmansend
+app.post('/mansendfile', (req, res) => {
+
+    console.log(req.body.password)
+    password_ricevuta = req.body.password;
+
+    if(password_ricevuta =="paperino")
+        res.send("<html>Bravo " + req.body.email + "<br> sono ponto a ricevere il file!</html>")
+
+    else
+        res.send("<html> attenzione password errata <html>")
+
+
+
+    });
+
+
+
+
+//esercizio 2------------------------------------------
